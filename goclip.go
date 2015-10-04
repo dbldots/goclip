ab3d13434c236928341ed895df25014a686f7b3f
johannes-kostas goetzinger <dbldots@gmail.com>
2015-10-04 18:28:06 +0200

    first commit

---
+++ goclip.go
@@ -0,0 +1,38 @@
+package main
+
+import "os"
+import "io/ioutil"
+import "fmt"
+
+func check(e error) {
+  if e != nil {
+    panic(e)
+  }
+}
+
+func contains(s []string, e string) bool {
+  for _, a := range s {
+    if a == e {
+        return true
+    }
+  }
+  return false
+}
+
+func main() {
+  argsWithoutProg := os.Args[1:]
+  homedir := os.Getenv("HOME")
+
+  clippath := fmt.Sprint(homedir, "/.goclip")
+  fmt.Print(clippath)
+
+  if contains(argsWithoutProg, "-i") {
+    bytes, _ := ioutil.ReadAll(os.Stdin)
+    err := ioutil.WriteFile(clippath, bytes, 0644)
+    check(err)
+  } else if contains(argsWithoutProg, "-o") {
+    dat, err := ioutil.ReadFile(clippath)
+    check(err)
+    fmt.Print(string(dat))
+  }
+}
